c8898a5dc873cef19ff4b5bc283af6892e8887c0
Earty4516 <worawit.4516@mail.kmutt.ac.th>
2021-04-26 21:05:15 +0700

    [ADD]Marven Project

---
+++ src/main/java/com/example/witpakul_back_end/WitPaKulBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.witpakul_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitPaKulBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitPaKulBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/witpakul_back_end/WitPaKulBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.witpakul_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitPaKulBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
