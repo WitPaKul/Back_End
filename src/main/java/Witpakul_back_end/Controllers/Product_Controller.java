c7d519c560d0b50abcd023c60e90cd4cf2d78a96
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-05-04 02:27:05 +0700

    [ADD] sample method for receive POST request from frontend

--- src/main/java/Witpakul_back_end/Controllers/Product_Controller.java
+++ src/main/java/Witpakul_back_end/Controllers/Product_Controller.java
@@ -2,9 +2,15 @@ package Witpakul_back_end.Controllers;
 import Witpakul_back_end.Models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import Witpakul_back_end.Repositories.Products_Repository;
 import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class Product_Controller {
     @Autowired
@@ -13,5 +19,11 @@ public class Product_Controller {
     @GetMapping("/all")
     public List<Product> listAllProducts(){
         return Products_Repository.findAll();
+    }
+    @PostMapping("/add_product")
+    public Product addProduct(@RequestBody Product newProduct) {
+        System.out.println(newProduct);
+        return newProduct;
+//        return Products_Repository.save(newEmployee);
     }
 }
