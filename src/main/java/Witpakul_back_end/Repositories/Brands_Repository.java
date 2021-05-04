57dd34e73f81ad3c2289f32e88ca9487dc3b07fc
Sakulthip Rassameecharoentham <sakulthip.r@mail.kmutt.ac.th>
2021-05-04 21:16:14 +0700

    [FIX] change variables name to be the same as in the database table

--- src/main/java/Witpakul_back_end/Repositories/Brands_Repository.java
+++ src/main/java/Witpakul_back_end/Repositories/Brands_Repository.java
@@ -1,5 +1,5 @@
 package Witpakul_back_end.Repositories;
 import Witpakul_back_end.Models.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface Brands_Repository extends JpaRepository<Brand, String> {
+public interface Brands_Repository extends JpaRepository<Brand, Integer> {
 }
