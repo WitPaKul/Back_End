395c62a0fc2d280aa49fe917ceaeff2be1bd79b4
Luck <pawat.rew@mail.kmutt.ac.th>
2021-05-10 03:51:19 +0700

    [Update] ExceptionHandle

--- src/main/java/Witpakul_back_end/Controllers/Image_Controller.java
+++ src/main/java/Witpakul_back_end/Controllers/Image_Controller.java
@@ -26,8 +26,6 @@ public class Image_Controller {
 
     private static final String IMAGE_PATH = "./public/pictures/";
 
-
-
     @GetMapping("/get/{id:.+}")
     public ResponseEntity<byte[]> getImage(@PathVariable("id")String id)  {
 
@@ -42,14 +40,14 @@ public class Image_Controller {
             e.printStackTrace(System.out);
             throw new ExceptionRequest("Image not Found OK Try it again");
         }
-        }
+    }
 
     @PostMapping ("/add/{id}")
-    public ResponseEntity<Object> uploadImage(@RequestParam("file") MultipartFile file, @PathVariable("id")String id) {
+    public ResponseEntity<Object> uploadImage(@RequestParam("file") MultipartFile file, @PathVariable("id")String id) throws IOException{
 
-        try {
-            // เพราะถ้าไม่ใส่ExceptionRequest ไม่เกิด
-            // + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")));
+            if (file.getOriginalFilename() == "") {
+                throw new ExceptionRequest("ADD Image not Found OK Try it again");
+            }
         File myFile = new File(IMAGE_PATH + id );
         System.out.println("Successfully");
         if(myFile.createNewFile()) {
@@ -59,19 +57,14 @@ public class Image_Controller {
 
         }
         return  new ResponseEntity<>("Uploaded Successfully", HttpStatus.OK);
-        } catch(Exception e) {
-
-            throw new ExceptionRequest("ADD Image not Found OK Try it again");
-        }
 
     }
-//เปลื่ยนทั้งฺbackend และ fronten Test ไม่ต้องใส่ .png
+            //เปลื่ยนทั้งฺbackend และ frontend Test ไม่ต้องใส่ .png
     @PutMapping("/edit/{id:.+}")
     public ResponseEntity<Object> changeImage(@RequestParam("file")MultipartFile file,@PathVariable("id")String id)   {
 
         try {
-            // เพราะถ้าไม่ใส่ExceptionRequest ไม่เกิด
-            // + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")));
+
             FileOutputStream fo = new FileOutputStream(IMAGE_PATH +  id   + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")));
             fo.write(file.getBytes());
             fo.close();
@@ -80,20 +73,6 @@ public class Image_Controller {
 
             throw new ExceptionRequest("edit Image not Found OK Try it again");
         }
-//        try {
-//        String IdString[] = id.split("\\.(?=[^\\.]+$)");
-//           int hasId = parseInt(IdString[0]);
-//            if(hasFoundId(hasId)){
-//                FileOutputStream fo = new FileOutputStream(IMAGE_PATH +  id  + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")));
-//                fo.write(file.getBytes());
-//                fo.close();
-//                return  new ResponseEntity<>("Change Successfully", HttpStatus.OK);
-//            }
-//        }  catch(Exception e) {
-//
-//            throw new ExceptionRequest("edit Image not Found OK Try it again");
-//        }
-//        return null;
 
     }
 
@@ -106,39 +85,9 @@ public class Image_Controller {
             }
         }catch (Exception e){
             System.out.println(e);
-
         }
         throw new ExceptionRequest("NOT Delete File");
 
-
-//        File myFile = new File(IMAGE_PATH + id  );
-//        myFile.delete();
-//
-//        System.out.println("OK Successfully");
-
-//            String IdString[] = id.split("\\.(?=[^\\.]+$)");
-//            int hasId = parseInt(IdString[0]);
-//            if (hasFoundId(hasId)) {
-//                File myFile = new File(IMAGE_PATH + id);
-//                myFile.delete();
-//
-//                System.out.println("OK Successfully");
-//            } else {
-//                System.out.println("NO delete");
-//            }
-//        return null;
     }
-
-
-//ใช้เชร็คว่า IDรูปเท่าproductไหม
-//    public boolean hasFoundId(int id){
-//        List<Product> product = Products_Repository.findAll();
-//        for (int i = 0; i < product.size(); i++) {
-//            if(product.get(i).getProduct_code() == id){
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
 
 }
